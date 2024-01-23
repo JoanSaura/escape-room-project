cd170d40ebe8f082ef30c48d1061de711a881767
JoanSaura <136345085+JoanSaura@users.noreply.github.com>
2024-01-23 22:35:03 +0100

    23.01

--- scripts/menus.js
+++ scripts/menus.js
@@ -1,15 +1,31 @@
-const botonUsuarios = document.querySelector('.gestion-usuarios');
-const menuUsuarios = document.getElementById('menu-usuarios');
-const cerrar = document.getElementById('Cerrar');
+document.addEventListener('DOMContentLoaded', function() {
+    const botonUsuarios = document.querySelector('.gestion-usuarios');
+    const menuUsuarios = document.getElementById('menu-usuarios');
+    const mostrarCrearUsuarioBtn = document.getElementById('crear-usuario-btn');
+    const crearUsuarioDiv = document.getElementById('crear-usuario');
+    const cerrarCrearUsuarioBtn = document.getElementById('cerrar-crear-usuario');
+    const cerrarMenuUsuarios = document.getElementById('cerrar-menu-usuarios');
 
-botonUsuarios.addEventListener('click', function() {
-    const menuEstaOculto = parseInt(getComputedStyle(menuUsuarios).right) < 0;
+    //Menu desplegable de Gestión de usuarios
+    botonUsuarios.addEventListener('click', function() {
+      const menuEstaOculto = parseInt(getComputedStyle(menuUsuarios).right) < 0;
+      menuUsuarios.style.right = menuEstaOculto ? '0' : '-250px';
+    });
+  
+    //Cerrar menu de usuarios
+    cerrarMenuUsuarios.addEventListener('click' ,function() {
+        const menuEstaOculto = parseInt(getComputedStyle(menuUsuarios).right) < 0;
+        menuUsuarios.style.right = menuEstaOculto ? '0' : '-250px';
+    });
 
-    menuUsuarios.style.right = menuEstaOculto ? '0' : '-250px';
-});
-
-cerrar.addEventListener('click', function() {
-    if (window.opener) {
-        window.close();
-    }
-});
+    //Mostrar Creación de usuarios
+    mostrarCrearUsuarioBtn.addEventListener('click', function() {
+      crearUsuarioDiv.style.display = 'flex';
+    });
+    
+    //Cerrar Creación de usuarios
+    cerrarCrearUsuarioBtn.addEventListener('click', function() {
+      crearUsuarioDiv.style.display = 'none';
+    });
+  });
+  
