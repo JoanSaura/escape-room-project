cd9a03f307d256c6eba883fd746c456d824b344a
TommyDrug <jsaura20489@iesjoanramis.org>
2024-02-15 20:49:39 +0100

    idk

--- juegos/Game002/script-002.js
+++ juegos/Game002/script-002.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const cronometro = document.getElementById("cronometro");
   const InterfazJuego = document.getElementById("interfaz-juego");
   const PuntosUser = document.getElementById("puntos-usuario");
-  const fondoSelector = document.querySelector(".fondo-selector"); 
+  const fondoSelector = document.querySelector(".fondo-selector");
   const palabraAdivinar = document.getElementById("palabra-adivinar");
   const erroresContainer = document.getElementById("contenedor-errores");
-  const teclasJuego = document.querySelectorAll('.tecla');
-  const letras = palabraAdivinar.querySelectorAll('.letra');
-  const juegoGanado = document.getElementById('juego-ganado');
-  const juegoPerdido = document.getElementById('juego-perdido');
-  const VolverHaIntentarlo = document.getElementById('volver-ha-intentarlo');
+  const teclasJuego = document.querySelectorAll(".tecla");
+  const letras = palabraAdivinar.querySelectorAll(".letra");
+  const juegoGanado = document.getElementById("juego-ganado");
+  const juegoPerdido = document.getElementById("juego-perdido");
+  const VolverHaIntentarlo = document.getElementById("volver-ha-intentarlo");
   //Variables globales
   let tiempoTranscurrido = 0;
   let intervalo;
@@ -48,7 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
     "pteronofobia",
     "consecotaleofobia",
   ];
-
 
   // Función para mostrar el interfaz de juego
   function mostrarInterfazJuego() {
@@ -112,13 +111,13 @@ document.addEventListener("DOMContentLoaded", function () {
   function generarLetrasPalabra(palabra) {
     palabraSeleccionada = palabra;
     console.log(palabra);
-     palabraAdivinar.innerHTML = ""; 
+    palabraAdivinar.innerHTML = "";
     for (let i = 0; i < palabra.length; i++) {
       const divLetra = document.createElement("div");
-      if (i < palabra.length) { 
-        divLetra.textContent = '*'; 
+      if (i < palabra.length) {
+        divLetra.textContent = "*";
       } else {
-        divLetra.textContent = palabra[i]; 
+        divLetra.textContent = palabra[i];
       }
       divLetra.classList.add("letra");
       palabraAdivinar.appendChild(divLetra);
@@ -129,7 +128,7 @@ document.addEventListener("DOMContentLoaded", function () {
     letra = letra.toLowerCase();
     if (palabraSeleccionada.includes(letra)) {
       // La letra es correcta
-      palabraAdivinar.querySelectorAll('.letra').forEach((divLetra, index) => {
+      palabraAdivinar.querySelectorAll(".letra").forEach((divLetra, index) => {
         if (palabraSeleccionada[index] === letra) {
           divLetra.textContent = letra;
           puntos += 10;
@@ -151,33 +150,29 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   }
-  
+
   // Agrega un evento de clic para cada tecla del teclado
-  teclasJuego.forEach(tecla => {
+  teclasJuego.forEach((tecla) => {
     tecla.addEventListener("click", function () {
       comprobarLetra(tecla.textContent, tecla);
-
     });
   });
-  
 
   //Crear el numero de errores maximo
   function generarErrores(MaxErrores) {
-    for(let i = 0; i<MaxErrores; i++) {
+    for (let i = 0; i < MaxErrores; i++) {
       const generadorError = document.createElement("div");
-      generadorError.className = 'error';
+      generadorError.className = "error";
       erroresContainer.appendChild(generadorError);
     }
   }
 
-    //Mostrar pantalla de derrota
-    function LanzarGameOver() {
-      detenerCronometro();
-      InterfazJuego.style.display = "none";
-      juegoPerdido.style.display = "block";
-    }
-  
-    
+  //Mostrar pantalla de derrota
+  function LanzarGameOver() {
+    detenerCronometro();
+    InterfazJuego.style.display = "none";
+    juegoPerdido.style.display = "block";
+  }
 
   //Mostrar pantalla de victoria
   function LanzarWin() {
@@ -189,7 +184,7 @@ document.addEventListener("DOMContentLoaded", function () {
   VolverHaIntentarlo.addEventListener("click", function () {
     location.reload();
   });
-  
+
   // Eventos click con generación de cartas y errores según la dificultad
   DificultadFacil.addEventListener("click", function () {
     dificultadElegida = "facil";
@@ -220,5 +215,4 @@ document.addEventListener("DOMContentLoaded", function () {
     const palabra = seleccionarPalabra(dificultadElegida);
     generarLetrasPalabra(palabra);
   });
-
 });
--- juegos/Game003/script-003.js
+++ juegos/Game003/script-003.js
@@ -4,10 +4,79 @@ document.addEventListener("DOMContentLoaded", function () {
   const DificultadMedia = document.getElementById("Avanzado");
   const DificultadDificil = document.getElementById("Locura");
   const cronometro = document.getElementById("cronometro");
-  const juegoGanado = document.getElementById('juego-ganado');
-  const juegoPerdido = document.getElementById('juego-perdido');
-  const VolverHaIntentarlo = document.getElementById('volver-ha-intentarlo');
+  const juegoGanado = document.getElementById("juego-ganado");
+  const juegoPerdido = document.getElementById("juego-perdido");
+  const VolverHaIntentarlo = document.getElementById("volver-ha-intentarlo");
   const erroresContainer = document.getElementById("contenedor-errores");
   const InterfazJuego = document.getElementById("interfaz-juego");
   const PuntosUser = document.getElementById("puntos-usuario");
+  
+  //Variables globales
+  let tiempoTranscurrido = 0;
+  let intervalo;
+  let puntos = 0;
+  let dificultadElegida = "";
+  let palabraSeleccionada = "";
+  let cantidadErrores = 0;
+
+  // Función para mostrar el interfaz de juego
+  function mostrarInterfazJuego() {
+    InterfazJuego.style.display = "block";
+  }
+
+  // Para cerrar el panel de dificultad y borrar el fondo
+  function ocultarPanelDificultad() {
+    panelDificultad.style.display = "none";
+    fondoSelector.remove();
+    mostrarInterfazJuego();
+  }
+
+  function iniciarCronometro() {
+    intervalo = setInterval(function () {
+      tiempoTranscurrido++;
+      actualizarCronometro();
+    }, 1000);
+  }
+
+  function detenerCronometro() {
+    clearInterval(intervalo);
+  }
+
+  function reiniciarCronometro() {
+    detenerCronometro();
+    tiempoTranscurrido = 0;
+    actualizarCronometro();
+  }
+
+  
+  // Actualizar el texto del cronometro
+  function actualizarCronometro() {
+    const horas = Math.floor(tiempoTranscurrido / 3600);
+    const minutos = Math.floor((tiempoTranscurrido % 3600) / 60);
+    const segundos = tiempoTranscurrido % 60;
+    cronometro.textContent = `${horas < 10 ? "0" + horas : horas}:${
+      minutos < 10 ? "0" + minutos : minutos
+    }:${segundos < 10 ? "0" + segundos : segundos}`;
+  }
+
+  //Recargue la pagina para volver a interntarlo
+  VolverHaIntentarlo.addEventListener("click", function () {
+    location.reload();
+  });
+
+  //Mostrar pantalla de victoria
+  function LanzarWin() {
+    detenerCronometro();
+    InterfazJuego.style.display = "none";
+    juegoGanado.style.display = "block";
+  }
+
+  //Mostrar pantalla de derrota
+  function LanzarGameOver() {
+    detenerCronometro();
+    InterfazJuego.style.display = "none";
+    juegoPerdido.style.display = "block";
+  }
+
+
 });
