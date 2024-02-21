40b011c3d27287bae9c700aa2ef34f78b3f1c11e
TommyDrug <jsaura20489@iesjoanramis.org>
2024-02-21 20:50:53 +0100

    21.02

--- scripts/scape.js
+++ scripts/scape.js
@@ -1,59 +1,76 @@
 document.addEventListener("DOMContentLoaded", function () {
-    //Capturamos los elementos del DOM
-    const subirNumerosBotones = document.querySelectorAll(".subir-numero");
-    const bajarNumerosBotones = document.querySelectorAll(".bajar-numero");
-    const numerosElementos = document.querySelectorAll(".numero");
-    const spanContrasena = document.getElementById("contraseña");
-    const comprobacionBtn = document.getElementById("comprobacion");
-    const pasarNota = document.getElementById('pasarNota');
-    const divNota = document.getElementById('nota');
-    const divContra = document.getElementById('contra');
-    //Generar aletoraiemnte la contraseña
-    function generarContrasena() {
-        const contrasena = Array.from({ length: 3 }, () => Math.floor(Math.random() * 9) + 1);
-        // Actualiza el elemento <span> con la contraseña generada
-        spanContrasena.textContent = contrasena.join("");
-        console.log("La contaseña es : ",contrasena.join(""));
+  const subirNumerosBotones = document.querySelectorAll(".subir-numero");
+  const bajarNumerosBotones = document.querySelectorAll(".bajar-numero");
+  const numerosElementos = document.querySelectorAll(".numero");
+  const body = document.querySelector("body");
+  const spanContrasena = document.getElementById("contraseña");
+  const comprobacionBtn = document.getElementById("comprobacion");
+  const pasarNota = document.getElementById("pasarNota");
+  const divNota = document.getElementById("nota");
+  const divContra = document.getElementById("contra");
+  const divFin = document.getElementById("fin");
+  const contrasena = Array.from(
+    { length: 3 },
+    () => Math.floor(Math.random() * 9) + 1
+  );
+
+  function generarContrasena() {
+    spanContrasena.textContent = contrasena.join("");
+    console.log("La contraseña es: ", contrasena.join(""));
+  }
+
+  function subirNumeros(index) {
+    let numeroActual = parseInt(numerosElementos[index].textContent);
+    if (numeroActual < 9) {
+      numeroActual++;
     }
-    //Para que funcione los clicks al subir los nombres
-    function subirNumeros(index) {
-        let numeroActual = parseInt(numerosElementos[index].textContent);
-        if (numeroActual < 9) {
-            numeroActual++;
-        }
-        numerosElementos[index].textContent = numeroActual;
+    numerosElementos[index].textContent = numeroActual;
+  }
+
+  function bajarNumeros(index) {
+    let numeroActual = parseInt(numerosElementos[index].textContent);
+    if (numeroActual > 0) {
+      numeroActual--;
     }
-     //Para que funcione los clicks al bajar los nombres
-    function bajarNumeros(index) {
-        let numeroActual = parseInt(numerosElementos[index].textContent);
-        if (numeroActual > 0) {
-            numeroActual--;
-        }
-        numerosElementos[index].textContent = numeroActual;
+    numerosElementos[index].textContent = numeroActual;
+  }
+
+  generarContrasena();
+
+  pasarNota.addEventListener("click", function () {
+    divNota.style.display = "none";
+    divContra.style.display = "flex";
+  });
+
+  comprobacionBtn.addEventListener("click", function () {
+    const numerosIngresados = Array.from(numerosElementos, (el) => parseInt(el.textContent));
+
+    if (JSON.stringify(numerosIngresados) === JSON.stringify(contrasena)) {
+      divContra.style.display = "none";
+
+      // Espera unos segundos antes de agregar la clase sueño
+      setTimeout(function () {
+        // Transición a fondo sueño
+        body.classList.add("sueño");
+
+        // Mostrar el div final después de unos segundos
+        setTimeout(function () {
+          divFin.classList.add("mostrar");
+        }, 1000);
+      }, 1000);
     }
-    
+  });
+  
 
-    //Generar contraseña
-    generarContrasena();
-
-    pasarNota.addEventListener('click',function() {
-        divNota.style.display = 'none';
-        divContra.style.display = 'flex'
-    })
-    comprobacionBtn.addEventListener("click", function () {
-
+  subirNumerosBotones.forEach((boton, index) => {
+    boton.addEventListener("click", function () {
+      subirNumeros(index);
     });
+  });
 
-    //Para que los numeros suban
-    subirNumerosBotones.forEach((boton, index) => {
-        boton.addEventListener("click", function () {
-            subirNumeros(index);
-        });
+  bajarNumerosBotones.forEach((boton, index) => {
+    boton.addEventListener("click", function () {
+      bajarNumeros(index);
     });
-    //Para que los numeros bajen
-    bajarNumerosBotones.forEach((boton, index) => {
-        boton.addEventListener("click", function () {
-            bajarNumeros(index);
-        });
-    });
+  });
 });
